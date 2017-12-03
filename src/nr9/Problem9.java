f354efc91f8b821dcde0ae9e1bf87266f8ead106
LauraRadu <laura_radu_1993@yahoo.com>
2017-12-03 20:53:35 +0200

    new

--- src/nr9/Problem9.java
+++ src/nr9/Problem9.java
@@ -24,12 +24,56 @@ at the hardware store.
 
 package nr9;
 
+import nr8.aa.Problem8;
+
+
 public class Problem9 {
     public static void main(String[] args) {
+        int number = Problem8.insertFromKeyboard("Insert 1 for square room, 2 for L room or 3 for round room.");
+        if(number==1 || number==2 || number==3) {
+            switch(number) {
+                case 1: squareRoom();
+                    break;
+                case 2: roomL();
+                    break;
+                case 3: roundRoom();
+                    break;
+            }
+        }
+        else{
+            System.exit(1);
+        }
 
     }
 
-    public static void input() {
+    public static void squareRoom() {
+        int width = Problem8.insertFromKeyboard("The width of your ceiling in feet? ");
+        int length = Problem8.insertFromKeyboard("The length of your ceiling in feet? ");
+        int area  = width * length;
+        calculus(area);
+    }
 
+    public static void roomL() {
+        int widthBig = Problem8.insertFromKeyboard("The biggest width of your ceiling in feet? ");
+        int widthSmall = Problem8.insertFromKeyboard("The smallest width of your ceiling in feet? ");
+        int lengthBig = Problem8.insertFromKeyboard("The biggest length of your ceiling in feet? ");
+        int lengthSmall = Problem8.insertFromKeyboard("The smallest length of your ceiling in feet? ");
+        int area = lengthBig*widthSmall + lengthSmall*(widthBig-widthSmall);
+        calculus(area);
+    }
+
+    public static void roundRoom() {
+        int diameter = Problem8.insertFromKeyboard("The diameter of the ceiling? ");
+        double radius = diameter/2.0;
+        final double pi = Math.PI;
+        double area = pi * radius * radius;
+
+        calculus(area);
+    }
+
+    public static void calculus(double area){
+        double oneGallon = 350.0;
+        double paintNeeded = Math.ceil(area/oneGallon);
+        System.out.println("The area of your ceiling is " + area + " square feet. You need " + paintNeeded + " gallons of paint for it.");
     }
 }
